2913cba529414bf06c21dee2f8f2090df91dfc81
Marius <marius.ihring@icloud.com>
2023-12-19 11:33:31 +0100

    location_card

--- app/locations/page.tsx
+++ app/locations/page.tsx
@@ -1,3 +1,11 @@
+'use client'
+
+import {LocationCard} from "@/components/locations/location_card";
+
 export default function Page() {
-	return <div>Locations</div>;
+	return (
+		<>
+		<LocationCard name={"test"} description={"test description"} is_starred={true} updated_at={"22.11.2022"} />
+		</>
+	);
 }
